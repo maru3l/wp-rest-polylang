3d882418b0916923f3b2f502a9b743136b1598dc
Marc-Antoine Ruel <hello@marcantoineruel.com>
2019-03-04 11:51:38 -0500

    change the plugin as class, make it work with custom post type and add validation

--- wp-rest-polylang.php
+++ wp-rest-polylang.php
@@ -8,45 +8,86 @@
  * Plugin URI: https://github.com/maru3l/wp-rest-polylang
  * License: gpl-3.0
  */
-function WP_REST_polylang_init() {
-    global $polylang;
 
-    if (isset($_GET['lang'])) {
-		$current_lang = $_GET['lang'];
 
-		$polylang->curlang = $polylang->model->get_language($current_lang);
+class WP_REST_polylang
+{
+
+	static $instance = false;
+
+	private function __construct() {
+		// Check if polylang is installed
+		require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+		if (!is_plugin_active('polylang/polylang.php')) {
+			return;
+		}
+
+		add_action('rest_api_init', array($this, 'init'), 0);
+	}
+
+	public static function getInstance() {
+		if ( !self::$instance )
+			self::$instance = new self;
+		return self::$instance;
+	}
+
+	public static function init() {
+		global $polylang;
+
+		if (isset($_GET['lang'])) {
+			$current_lang = $_GET['lang'];
+
+			$polylang->curlang = $polylang->model->get_language($current_lang);
+		}
+
+		$post_types = get_post_types( array( 'public' => true ), 'names' );
+
+		foreach( $post_types as $post_type ) {
+			if (pll_is_translated_post_type( $post_type )) {
+				self::register_api_field($post_type);
+			}
+		}
+	}
+
+	public function register_api_field($post_type) {
+		register_rest_field(
+			$post_type,
+			"polylang_current_lang",
+			array(
+				"get_callback" => array( $this, "get_current_lang" ),
+				"schema" => null
+			)
+		);
+
+		register_rest_field(
+			$post_type,
+			"polylang_translations",
+			array(
+				"get_callback" => array( $this, "get_translations"  ),
+				"schema" => null
+			)
+		);
+	}
+
+	public function get_current_lang( $object ) {
+		return pll_get_post_language($object['id'], 'locale');
+	}
+
+	public function get_translations( $object ) {
+		$translations = pll_get_post_translations($object['id']);
+
+		return array_reduce($translations, function ($carry, $translation) {
+			$item = array(
+				'locale' => pll_get_post_language($translation, 'locale'),
+				'id' => $translation
+			);
+
+			array_push($carry, $item);
+
+			return $carry;
+		}, array());
 	}
 }
 
-add_action('rest_api_init', 'WP_REST_polylang_init');
-
-function WP_REST_polylang_prepare_post($data, $post, $context) {
-	$ID = $post->ID;
-	$locale = pll_get_post_language($ID, 'locale');
-	$translations = pll_get_post_translations($ID);
-
-	$translations = array_filter($translations, function ($translation) use ($ID) {
-		return ($translation !== $ID);
-	});
-
-	$translations = array_reduce($translations, function ($carry, $translation) {
-		$item = array(
-			'locale' => pll_get_post_language($translation, 'locale'),
-			'id' => $translation
-		);
-
-		array_push($carry, $item);
-
-		return $carry;
-	}, array());
-
-	$data->data['polylang_current_lang'] = (string) $locale;
-
-	$data->data['polylang_translations'] = (array) $translations;
-
-	return $data;
-}
-
-add_filter('rest_prepare_post', 'WP_REST_polylang_prepare_post', 10, 3);
-
-add_filter('rest_prepare_page', 'WP_REST_polylang_prepare_post', 10, 3);
+$WP_REST_polylang = WP_REST_polylang::getInstance();
